8fd666a0acdc85d2fef1290785d8ca7e5d434f0a
powcoder代写 <powcoder@163.com>
2022-07-09 14:29:59 +0800

    程序代写 加微信 powcoder

---
+++ lab2.scala
@@ -0,0 +1,23 @@
+object Lab2 {
+
+   def upper(mystrings: Vector[String]) = {
+      //code in here
+   }
+
+   def su(mystrings: Vector[String]) = {
+       //code in here
+   }
+
+   def tot(mystrings: Vector[String]) = {
+     //code in here
+   }
+
+   def evaluate(r: Node): Double = {
+      r match {
+        //put stuff in here
+        case _ => throw new Exception("I don't know") //this case catches everything else
+      }
+
+   }
+
+}
